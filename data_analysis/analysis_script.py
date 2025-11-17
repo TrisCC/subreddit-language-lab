9dfa07566f277f063fab8d07f5666b37c94c9aac
TrisCC <tristancotino@gmail.com>
2025-11-17 11:20:47 +0100

    Add analysis step

--- data_analysis/analysis_script.py
+++ data_analysis/analysis_script.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import pandas as pd
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
 
 def main():
     print("==== START ====")
 
+    print("==== LOADING FILES ====")
     # Define the path to the data_raw directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_raw_dir = os.path.abspath(os.path.join(current_dir, '..', 'data_raw'))
@@ -29,27 +32,32 @@ def main():
         csv_files.remove('50_subreddits_list.csv')
 
     # Print the list of CSV files
-    print("CSV files:")
-    for file in csv_files:
-        print(f"- {file}")
+    # print("CSV files:")
+    # for file in csv_files:
+    #     print(f"- {file}")
 
+    # Iterate through each CSV file and load it into a DataFrame
     for file in csv_files:
         file_path = os.path.join(reddit_data_dir, file)
         df = load_csv(file_path)
         if df is not None:
-            print(f"First 5 rows of {file}:")
-            print(df.head())
+            print(f"==== ANALYZING {file} ====")
+            analyze_data(df)
 
     print("==== END ====")
 
 def load_csv(file_path):
     try:
         df = pd.read_csv(file_path)
-        print(f"Loaded {file_path} with shape {df.shape}")
+        # print(f"Loaded {file_path} with shape {df.shape}")
         return df
     except Exception as e:
         print(f"Error loading {file_path}: {e}")
         return None
+    
+def analyze_data(df):
+    # Example analysis: Print the first 5 rows of the DataFrame
+    print(df.head())
 
 if __name__ == "__main__":
     main()
