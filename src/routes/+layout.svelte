4fb93da74d2fd05a81ee8bbc509892e06487f822
TrisCC <tristancotino@gmail.com>
2025-12-02 15:06:49 +0100

    Prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 
   let { children } = $props();
   import "../app.css";
+
+  // If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+  // pages by setting this here, but should prerender as many as possible to
+  // avoid large performance and SEO impacts
+  export const prerender = true;
 </script>
 
 <svelte:head>
