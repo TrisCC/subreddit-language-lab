81dd150cd8ecdc2acc4e89a2b9e763ae124a46b1
TrisCC <tristancotino@gmail.com>
2026-01-02 16:54:11 +0100

    Color scheme

--- src/lib/WordCloud.svelte
+++ src/lib/WordCloud.svelte
@@ -59,7 +59,7 @@
         .append("text")
         .style("font-size", (d) => `${d.size}px`)
         .style("font-family", "Impact")
-        .style("fill", (d, i) => d3.schemeCategory10[i % 10])
+        .style("fill", (d, i) => d3.interpolatePlasma(i / 20))
         .attr("text-anchor", "middle")
         .attr("transform", (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
         .text((d) => d.text || "");
